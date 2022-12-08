7002c8abaac1e6b8f33eec14548baad20875d355
DroneMaker <59488688+DroneMaker123@users.noreply.github.com>
2022-12-08 16:41:34 +0100

    Make MainMenu and Settings scenes with canvas and menu buttons. Add LevelManager GameObject that controls changing the scene.

---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
